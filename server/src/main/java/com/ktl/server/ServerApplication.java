0742a39b9e0641c62f9fd238111c6b78a6b78451
KieuTriLang <2414lq@gmail.com>
2022-11-21 08:50:44 +0700

    init project

---
+++ server/src/main/java/com/ktl/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.ktl.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
